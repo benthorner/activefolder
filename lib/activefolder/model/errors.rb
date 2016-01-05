4883100e87241bdc397b76bb2759eedb5688ba2e
Ben Thorner <ben.thorner@bt.com>
2016-01-05 13:07:35 +0000

    Add messages for model errors

--- lib/activefolder/model/errors.rb
+++ lib/activefolder/model/errors.rb
@@ -2,10 +2,22 @@ require 'activefolder/errors'
 
 module ActiveFolder
   module Model
-    class MissingAttributeError < Error; end
-    class TypeError < Error; end
+    class MissingAttributeError < Error
+      def initialize(attr)
+        super("Missing attribute #{attr}")
+      end
+    end
 
-    class DuplicateError < Error; end
-    class NotFoundError < Error; end
+    class DuplicateError < Error
+      def initialize(name)
+        super("Duplicate object #{name}")
+      end
+    end
+
+    class NotFoundError < Error
+      def initialize(name)
+        super("Object not found #{name}")
+      end
+    end
   end
 end
